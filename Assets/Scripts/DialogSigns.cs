e39c26e791fd2fd6dfa0739eddbd96f190a2a5d7
sl1per <slipviegas@gmail.com>
2020-11-08 01:24:51 +0100

    Dialog system

---
+++ Assets/Scripts/DialogSigns.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogSigns : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(/*Input.GetKeyDown(KeyCode.Space) && */ playerInRange)
+        {
+            dialogBox.SetActive(true);
+            dialogText.text = dialog;
+        } else
+        {
+            dialogBox.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        playerInRange = false;
+        dialogBox.SetActive(false);
+    }
+
+}
