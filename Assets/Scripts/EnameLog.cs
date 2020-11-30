953005e50e34631e7e6184ecf2bbd148c9fcc020
sl1per <slipviegas@gmail.com>
2020-11-30 01:23:30 +0100

    enemy nimations and remove Tiled2Unity plugin

--- Assets/Scripts/EnameLog.cs
+++ Assets/Scripts/EnameLog.cs
@@ -36,8 +36,46 @@ public class EnameLog : Enemy
             {
 
                 Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                ChangeAnim(temp - transform.position);
                 myRigidbody.MovePosition(temp);
                 ChangeState(EnemyState.walk);
+                anim.SetBool("wakeUp", true);
+            } 
+        }
+        else if(Vector3.Distance(target.position, transform.position) > chaseRadius)
+        {
+            anim.SetBool("wakeUp", false);
+        }
+    }
+
+    private void SetAnimFloat(Vector2 setVector)
+    {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+
+    private void ChangeAnim(Vector2 direction)
+    {
+        if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if(direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else if(direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
+        }
+        else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if(direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else if(direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
             }
         }
     }
